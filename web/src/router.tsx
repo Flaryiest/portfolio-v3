781d83e12fa26bdc1fd245bbb9356b0021dd0e22
Flaryiest <ericmzuo1@gmail.com>
2025-12-23 09:37:58 -0700

    base setup

---
+++ web/src/router.tsx
@@ -0,0 +1,10 @@
+import {createBrowserRouter, createRoutesFromElements, Route} from "react-router-dom";
+import Index from "./pages/index";
+
+export const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path="/">
+            <Route index element={<Index />} />
+        </Route>
+    )
+);
