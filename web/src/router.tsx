853d95a2472ccbb5794095e55e9ddb011947f55f
Flaryiest <ericmzuo1@gmail.com>
2025-12-24 13:15:10 -0700

    routing set up

--- web/src/router.tsx
+++ web/src/router.tsx
@@ -4,11 +4,14 @@ import {
     Route,
 } from 'react-router-dom'
 import Index from './pages/index'
-
+import About from './pages/about/about'
+import NotFound from './pages/404/404'
 export const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path="/">
             <Route index element={<Index />} />
+            <Route path="about" element={<About />} />
+            <Route path="*" element={<NotFound />} />
         </Route>
     )
 )
