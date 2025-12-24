136671181272f3ea44f0ee5ccc5697cb70c7deec
Flaryiest <ericmzuo1@gmail.com>
2025-12-24 12:38:57 -0700

    project cards

--- web/src/components/projectCard/projectCard.tsx
+++ web/src/components/projectCard/projectCard.tsx
@@ -1,18 +1,53 @@
-import styles from "./projectCard.module.css"
+import styles from './projectCard.module.css'
 
-export default function ProjectCard({company, title, description, link, startDate, endDate}: {company: string, title: string, description: string, link: string, startDate: string, endDate: string}) {
-    return <>
-        <div className={styles.cardContainer}>
-            <div className={styles.card}>
-                <div className={styles.cardDate}>
-                    <p>{startDate} - {endDate}</p>
-                </div>
-                <div className={styles.cardContent}>
-                    <a href={link}><h3 className={styles.cardCompany}>{company}</h3></a>
-                    <h4 className={styles.cardTitle}>{title}</h4>
-                    <p className={styles.cardDescription}>{description}</p>
+export default function ProjectCard({
+    company,
+    title,
+    description,
+    link,
+    startDate,
+    endDate,
+}: {
+    company: string
+    title: string
+    description: string
+    link: string
+    startDate: string
+    endDate: string
+}) {
+    return (
+        <>
+            <div className={styles.cardContainer}>
+                <div className={styles.card}>
+                    <div className={styles.cardDate}>
+                        <p className={styles.date}>
+                            {startDate} - {endDate}
+                        </p>
+                    </div>
+                    <div className={styles.cardContent}>
+                        <a href={link} className={styles.companyLink}>
+                            <h3 className={styles.cardCompany}>{company}</h3>
+                            <svg
+                                className={styles.arrow}
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="18"
+                                height="18"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                stroke="currentColor"
+                                strokeWidth="2"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                            >
+                                <line x1="7" y1="17" x2="17" y2="7"></line>
+                                <polyline points="7 7 17 7 17 17"></polyline>
+                            </svg>
+                        </a>
+                        <h4 className={styles.cardTitle}>{title}</h4>
+                        <p className={styles.cardDescription}>{description}</p>
+                    </div>
                 </div>
             </div>
-        </div>
-    </>
+        </>
+    )
 }
--- web/src/pages/index/index.tsx
+++ web/src/pages/index/index.tsx
@@ -1,7 +1,6 @@
 import styles from './index.module.css'
 import { Link } from 'react-router-dom'
-import ProjectCard from "../../components/projectCard/projectCard"
-
+import ProjectCard from '../../components/projectCard/projectCard'
 
 export default function Index() {
     return (
@@ -9,7 +8,6 @@ export default function Index() {
             <div className={styles.page}>
                 <div className={styles.leftWall}></div>
                 <div className={styles.centered}>
-                    
                     <header className={styles.header}>
                         <h1 className={styles.title}>Eric Zuo</h1>
                         <nav className={styles.nav}>
@@ -45,21 +43,42 @@ export default function Index() {
                         <article className={styles.intro}>
                             <p>Hello, I'm Eric Zuo!</p>
                             <br></br>
-                            <p>I'm a student in the IB Diploma Programme at Sir Winston Churchill High School.</p>
-                            <p>Currently building Strand, an AI travel agent that adapts your preferences and takes you through the decision making process.</p>
+                            <p>
+                                I'm a student in the IB Diploma Programme at Sir
+                                Winston Churchill High School.
+                            </p>
+                            <p>
+                                Currently building Strand, an AI travel agent
+                                that adapts your preferences and takes you
+                                through the decision making process.
+                            </p>
                             <br></br>
-                            <p>In my free time, I enjoy reading, writing, and going for long walks. Please feel free to say hi, check out my code, or view my experience.</p>
+                            <p>
+                                In my free time, I enjoy reading, writing, and
+                                going for long walks. Please feel free to say
+                                hi, check out my code, or view my experience.
+                            </p>
                             <br></br>
-                            <p>I have worked in both 2D and 3D, with companies ranging from nonprofits to YC backed startups. Now I want to work with you.</p>
-
+                            <p>
+                                I have worked in both 2D and 3D, with companies
+                                ranging from nonprofits to YC backed startups.
+                                Now I want to work with you.
+                            </p>
                         </article>
                     </div>
                     <div className={styles.projects}>
-                        <ProjectCard company="Magic Hour (YC W24)" title="Software Engineer Intern" description="Magic Hour is an AI platform that accelerates content creation for both the average person, and companies alike. As a software engineer intern at Magic Hour, I have had the pleasure of working on a variety of different AI features. One of my features currently in prod is the AI GIF generator, used >100x a day. Go check us out." link="https://magichour.ai" startDate="August 2025" endDate="December 2025" />
+                        <ProjectCard
+                            company="Magic Hour (YC W24)"
+                            title="Software Engineer Intern"
+                            description="Magic Hour is an AI platform that accelerates content creation for both the average person, and companies alike. As a software engineer intern at Magic Hour, I have had the pleasure of working on a variety of different AI features. One of my features currently in prod is the AI GIF generator, used >100x a day. Go check us out."
+                            link="https://magichour.ai"
+                            startDate="AUG 2025"
+                            endDate="DEC 2025"
+                        />
                     </div>
                 </div>
                 <div className={styles.rightWall}></div>
             </div>
-        </> 
+        </>
     )
 }
