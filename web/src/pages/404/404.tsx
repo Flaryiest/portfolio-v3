7960183e5d2ea5ee2e5b4c41c6364c46643a1a93
Flaryiest <ericmzuo1@gmail.com>
2025-12-25 18:45:30 -0700

    added a simple 404 page

--- web/src/pages/404/404.tsx
+++ web/src/pages/404/404.tsx
@@ -1,4 +1,9 @@
 import styles from './404.module.css'
 export default function NotFoundPage() {
-    return <></>
+    return <div className={styles.page}>
+        <h1 className={styles.title}>404 - Page Not Found</h1>
+        <p className={styles.description}>
+            Oops! The page you're looking for doesn't exist.
+        </p>
+    </div>
 }
