538ffee1ba6f89d332eea917c32755fd8d4b6df0
Flaryiest <ericmzuo1@gmail.com>
2025-12-25 17:49:06 -0700

    finished projects section

--- web/src/components/projectCard/projectCard.tsx
+++ web/src/components/projectCard/projectCard.tsx
@@ -1,14 +1,16 @@
 import styles from "./projectCard.module.css"
-export default function ProjectCard({imgSrc, title, description, technologies, url}: {imgSrc: string, title: string, description: string, technologies: string[], url: string}) {
-    return <>
-        <div className={styles.cardContainer}>
+export default function ProjectCard({imgSrc, title, description, technologies, url, year}: {imgSrc: string, title: string, description: string, technologies: string[], url: string, year: string}) {
+    return <div className={styles.cardContainer}>
             <div className={styles.card}>
                 <div className={styles.imageContainer}>
-                    <img src={imgSrc} alt={title} className={styles.cardImage} />
+                    <img src={imgSrc} alt={title} className={styles.image} />
                 </div>
-                <div className={styles.cardContent}>
-                    <h2>{title}</h2>
-                    <p className={styles.cardDescription}>{description}</p>
+                <div className={styles.content}>
+                    <div className={styles.titleContainer}>
+                        <h2 className={styles.title}>{title}</h2>
+                        <span className={styles.year}>{year}</span>
+                    </div>
+                    <p className={styles.description}>{description}</p>
                     <div className={styles.techContainer}>
                         {technologies.join(", ")}
                     </div>
@@ -18,5 +20,5 @@ export default function ProjectCard({imgSrc, title, description, technologies, u
                 </div>
             </div>
         </div>
-    </>
+    
 }
--- web/src/pages/projects/projects.tsx
+++ web/src/pages/projects/projects.tsx
@@ -1,5 +1,8 @@
+import styles from './projects.module.css'
+import ProjectCard from '../../components/projectCard/projectCard'
 export default function Projects() {
-    return <div>
-        Test
+    return <div className={styles.projects}>
+        <ProjectCard imgSrc="/projects/strand.webp" title="Strand" description="An AI travel assistant that learns your preferences" technologies={["TypeScript", "React", "PostgreSQL", "Prisma", "Node", "MCP", "RAG"]} url="https://usestrand.space/" year="2025" />
+        <ProjectCard imgSrc="/projects/mythea.webp" title="Mythea" description="Cursor for schoolwork" technologies={["Python", "TypeScript", "React",  "PostgreSQL", "Redis"]} url="https://mythea.io/" year="2025" />
     </div>
 }
